3127f8061c05ef1cdcf2cd4f1c8d050e697896eb
Ashu Prakash <aspksh@gmail.com>
2026-09-02 15:59:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -387,9 +387,7 @@ if uploaded_file is not None:
                 )
             ):
 
-                with st.expander(
-                    f"Chunk {i + 1} | Distance: {distance:.4f}"
-                ):
+                with st.expander(f"Chunk {i + 1} | Distance: {distance:.4f}"):
 
                     #st.write(chunk)
 
