339d698f77b6ab896862bb650f58cebf948f69d1
Ashu Prakash <aspksh@gmail.com>
2026-09-02 17:02:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -283,7 +283,7 @@ Answer:
     # ========================================================
 
     response = client.models.generate_content(
-        model = "gemini-3.6-flash",
+        model = "gemma-4-31b-it",
         contents=prompt
     )
 
